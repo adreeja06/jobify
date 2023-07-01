4ce9750f2e2a2a81a42204421ddddebcffc91205
Adreeja <adreejamahato@gmail.com>
2023-07-02 02:23:16 +0530

    init

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
+
+//db and authenticateUser
 import connectDB from './db/connect.js'
+
+//routers
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
+
 
 //middleware
 
 import NotFoundMiddleWare from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
     throw new Error('This is a new error')
     res.send('Welcome!')
   })
   
+  app.use('/api/v1/auth', authRouter)
+  app.use('/api/v1/jobs', jobsRouter)
+
   app.use(NotFoundMiddleWare)
   app.use(errorHandlerMiddleware)
   const port = process.env.PORT || 5000
